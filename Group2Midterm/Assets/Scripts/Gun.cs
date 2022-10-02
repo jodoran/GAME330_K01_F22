c357ccc91eda55dfa01bf6db3e77ffa1d6938a15
Zachary Peters <zacharyspeters@gmail.com>
2022-10-03 01:39:00 +0900

    Update 2
    Added some scripts to the project for the gun and enemy. Added crosshairs. Added a new gun model.

---
+++ Group2Midterm/Assets/Scripts/Gun.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("space"))
+        {
+
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+    }
+}
