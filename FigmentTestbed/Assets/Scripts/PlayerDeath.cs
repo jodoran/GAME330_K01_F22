256e25fa1fe90afc16dfd8aa8a92a288714517ac
Zachary Peters <zacharyspeters@gmail.com>
2022-10-17 16:05:04 +0900

    Update 7
    Put finishing touches on the game with adding some more sound and music. I am attempting to figure out the end screen but the title screen is finished so we have at least that done.

--- FigmentTestbed/Assets/Scripts/PlayerDeath.cs
+++ FigmentTestbed/Assets/Scripts/PlayerDeath.cs
@@ -1,30 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    //public GameObject enemy;
-    //public GameObject player;
+    public GameObject endgameCanvas;
+    public GameObject gameCanvas;
+    public AudioSource backgroundMusic;
+    public AudioSource endgameSound;
+    private bool gameOver = false;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
             Debug.Log("DEATH");
-            SceneManager.LoadScene("EndScreen");
+            gameOver = true;
+            gameCanvas.SetActive(false);
+            endgameCanvas.SetActive(true);
+            backgroundMusic.Stop();
+            endgameSound.Play();
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        endgameCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("space") && gameOver == true)
+        {
+            SceneManager.LoadScene("FigmentTestScene");
+        }
     }
 }
